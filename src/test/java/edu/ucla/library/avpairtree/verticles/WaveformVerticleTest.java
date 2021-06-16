07f89ad84b880d5556078bcb581582e0d7108b8b
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2021-06-16 16:33:49 -0700

    Use Vert.x WebClient instead of HttpClient

--- src/test/java/edu/ucla/library/avpairtree/verticles/WaveformVerticleTest.java
+++ src/test/java/edu/ucla/library/avpairtree/verticles/WaveformVerticleTest.java
@@ -2,8 +2,6 @@ package edu.ucla.library.avpairtree.verticles;
 
 import static org.junit.Assert.assertEquals;
 
-import java.net.URI;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -15,11 +13,11 @@ import edu.ucla.library.avpairtree.MessageCodes;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
-import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.ext.web.client.WebClient;
 
 /**
  * Tests the waveform verticle.
@@ -50,31 +48,22 @@ public class WaveformVerticleTest extends AbstractAvPtTest {
         // TODO: mock the S3 bucket
         vertx.eventBus().<JsonObject>request(WaveformVerticle.class.getName(), csvItem).onSuccess(transformation -> {
             // Compare the data retrieved from S3 with a local test fixture
-            final URI audiowaveformURL = URI.create(transformation.body().getString(csvItem.getItemARK()));
-            final String host = audiowaveformURL.getHost();
-            final String path = audiowaveformURL.getPath();
+            final String audiowaveformURL = transformation.body().getString(csvItem.getItemARK());
 
-            vertx.createHttpClient().request(HttpMethod.GET, host, path).onSuccess(req -> {
-                req.send().onSuccess(response -> {
-                    response.body().onSuccess(resp -> {
-                        final Buffer expected =
-                                vertx.fileSystem().readFileBlocking("src/test/resources/soul/audio/uclapasc.dat");
-                        final Buffer actual = resp;
+            WebClient.create(vertx).getAbs(audiowaveformURL).send().onSuccess(resp -> {
+                final Buffer expected =
+                        vertx.fileSystem().readFileBlocking("src/test/resources/soul/audio/uclapasc.dat");
+                final Buffer actual = resp.body();
 
-                        try {
-                            assertEquals(expected, actual);
-                        } catch (final AssertionError details) {
-                            LOGGER.error(details, details.getMessage());
-                            aContext.fail();
-                        } finally {
-                            // TODO: clean up the S3 bucket
-                            asyncTask.complete();
-                        }
-                    });
-                }).onFailure(error -> {
-                    LOGGER.error(error, error.getMessage());
+                try {
+                    assertEquals(expected, actual);
+                } catch (final AssertionError details) {
+                    LOGGER.error(details, details.getMessage());
                     aContext.fail();
-                });
+                } finally {
+                    // TODO: clean up the S3 bucket
+                    asyncTask.complete();
+                }
             }).onFailure(error -> {
                 LOGGER.error(error, error.getMessage());
                 aContext.fail();
