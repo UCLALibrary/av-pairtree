0bda78cd3693fe9df6cca90cc9d4efab47d42928
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2022-04-14 14:48:28 -0700

    [SERV-421] Use designated temp directory to write converted files

--- src/main/java/edu/ucla/library/avpairtree/AvPtConstants.java
+++ src/main/java/edu/ucla/library/avpairtree/AvPtConstants.java
@@ -17,11 +17,6 @@ public final class AvPtConstants {
     public static final String JSON = "application/json";
 
     /**
-     * The system's temporary files directory.
-     */
-    public static final String SYSTEM_TMP_DIR = System.getProperty("java.io.tmpdir");
-
-    /**
      * The event bus address of the consumer of waveform data (typically an Amazon S3 client, but sometimes a Localstack
      * S3 client for integration testing).
      */
